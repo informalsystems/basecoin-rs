08f5538a559d2cecaea1af49b6583d1352f320b6
Thane Thomson <connect@thanethomson.com>
2021-06-09 07:35:52 -0400

    Expand validation debugging to manually handle MsgSend decoding
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- src/app/handle.rs
+++ src/app/handle.rs
@@ -6,7 +6,10 @@ use crate::sync::{channel_recv, channel_send};
 use cosmos_sdk::bank::MsgSend;
 use cosmos_sdk::tx::MsgType;
 use cosmos_sdk::{AccountId, Coin, Tx};
+use prost::Message;
+use prost_types::Any;
 use std::collections::HashMap;
+use std::convert::TryFrom;
 use std::str::FromStr;
 use std::sync::mpsc::{channel, Sender};
 use tendermint_abci::{Application, Result};
@@ -185,18 +188,35 @@ fn validate_tx<B: AsRef<[u8]>>(tx: B) -> std::result::Result<MsgSend, (u32, Stri
         debug!("Got empty tx body");
         return Err((2, "no messages in incoming transaction".to_string()));
     }
-    let msg = match MsgSend::from_msg(&tx.body.messages[0]) {
+    let msg_any = Any::from(tx.body.messages[0].clone());
+    debug!("Got Protobuf Any message: {:?}", msg_any);
+    if msg_any.type_url != "/cosmos.bank.v1beta1.MsgSend" {
+        return Err((
+            3,
+            format!(
+                "expected message type \"/cosmos.bank.v1beta1.MsgSend\", but got \"{}\"",
+                msg_any.type_url
+            ),
+        ));
+    }
+    let proto_msg_send: cosmos_sdk_proto::cosmos::bank::v1beta1::MsgSend =
+        match Message::decode(msg_any.value.as_ref()) {
+            Ok(m) => m,
+            Err(e) => return Err((4, e.to_string())),
+        };
+    debug!("Protobuf MsgSend: {:?}", proto_msg_send);
+    let msg = match MsgSend::try_from(proto_msg_send) {
         Ok(m) => m,
         Err(e) => {
             debug!(
                 "Failed to decode a bank send tx from {:?}\n\n{:?}",
                 tx.body.messages[0], e
             );
-            return Err((3, e.to_string()));
+            return Err((5, e.to_string()));
         }
     };
     if let Err(e) = u64::from_str(msg.amount[0].amount.to_string().as_str()) {
-        return Err((4, format!("failed to decode amount: {}", e.to_string())));
+        return Err((6, format!("failed to decode amount: {}", e.to_string())));
     }
     Ok(msg)
 }
