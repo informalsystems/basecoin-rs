e721498a5a26e3455307b68dfdf427d212b816f5
Shoaib Ahmed <sufialhussaini@gmail.com>
2021-06-14 16:25:38 +0530

    Add test for transfer

--- src/app/state.rs
+++ src/app/state.rs
@@ -46,7 +46,7 @@ impl Default for Store {
     }
 }
 
-#[derive(Debug, Clone, Error)]
+#[derive(Debug, Clone, Error, PartialEq)]
 pub enum StateError {
     #[error("no such account with ID {0}")]
     NoSuchAccount(String),
@@ -143,12 +143,9 @@ impl BaseCoinState {
 
         for (amount, denom) in amounts {
             let mut src_balance = match src_balances.get(&denom) {
-                Some(b) => *b,
-                None => return Err(StateError::InsufficientSourceFunds.into()),
+                Some(b) if *b >= amount => *b,
+                _ => return Err(StateError::InsufficientSourceFunds.into()),
             };
-            if src_balance < amount {
-                return Err(StateError::InsufficientSourceFunds.into());
-            }
             let mut dest_balance = dest_balances.get(&denom).map(Clone::clone).unwrap_or(0_u64);
             if dest_balance > u64::MAX - amount {
                 return Err(StateError::DestFundOverflow.into());
@@ -190,4 +187,156 @@ impl BaseCoinState {
         encode_varint(self.store.len() as u64, &mut hash);
         hash.to_vec()
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::{from_value, json};
+
+    #[inline]
+    fn create_coin(denom: &str, amount: u64) -> Result<Coin> {
+        use cosmos_sdk::{Decimal, Denom};
+        Ok(Coin {
+            denom: Denom::from_str(denom)?,
+            amount: Decimal::from(amount),
+        })
+    }
+
+    #[test]
+    fn test_transfer() -> Result<()> {
+        let app_state = json!({
+          "cosmos1snd5m4h0wt5ur55d47vpxla389r2xkf8dl6g9w": { "basecoin" : 1000, "othercoin" : 1000 },
+          "cosmos1t2e0nyjhwn3revunvf2uperhftvhzu4euuzva9": { "basecoin" : 250, "othercoin" : 5000 },
+          "cosmos1uawm90a5xm36kjmaazv89nxmfr8s8cyzkjqytd": { "acidcoin": 500 },
+          "cosmos1ny9epydqnr7ymqhmgfvlshp3485cuqlmt7vsmf": { },
+          "cosmos1xwgdxu4ahd9eevtfnq5f7w4td3rqnph4llnngw": { "acidcoin": 500, "basecoin" : 0, "othercoin": 100 },
+          "cosmos1mac8xqhun2c3y0njptdmmh3vy8nfjmtm6vua9u": { "basecoin" : 1000 },
+          "cosmos1wkvwnez6fkjn63xaz7nzpm4zxcd9cetqmyh2y8": { "basecoin" : 1 },
+          "cosmos166vcha998g7tl8j8cq0kwa8rfvm68cqmj88cff": { "basecoin" : u64::MAX }
+        });
+        let mut state = BaseCoinState {
+            store: from_value(app_state)?,
+            ..Default::default()
+        };
+
+        // single denom transfer
+        let from = "cosmos1snd5m4h0wt5ur55d47vpxla389r2xkf8dl6g9w";
+        let to = "cosmos1t2e0nyjhwn3revunvf2uperhftvhzu4euuzva9";
+        let denom_basecoin = "basecoin";
+        assert!(state
+            .transfer(from, to, vec![create_coin(denom_basecoin, 100)?])
+            .is_ok());
+        assert_eq!(state.get_balances(from).unwrap()[denom_basecoin], 900);
+        assert_eq!(state.get_balances(to).unwrap()[denom_basecoin], 350);
+
+        // insufficient source funds
+        assert_eq!(
+            state
+                .transfer(from, to, vec![create_coin(denom_basecoin, 901)?],)
+                .err()
+                .unwrap()
+                .downcast::<StateError>()?,
+            StateError::InsufficientSourceFunds
+        );
+
+        // unknown denom transfer
+        assert_eq!(
+            state
+                .transfer(from, to, vec![create_coin("unknowncoin", 10)?],)
+                .err()
+                .unwrap()
+                .downcast::<StateError>()?,
+            StateError::InsufficientSourceFunds
+        );
+
+        // transfer from non-existent account
+        let non_existent_acc = "cosmos1awx9pmmr82p7097wx6gft923k4ugwd5vxuhf3s";
+        assert_eq!(
+            state
+                .transfer(non_existent_acc, to, vec![create_coin(denom_basecoin, 10)?],)
+                .err()
+                .unwrap()
+                .downcast::<StateError>()?,
+            StateError::NoSuchAccount(non_existent_acc.to_owned())
+        );
+
+        // transfer to non-existent account
+        assert!(state
+            .transfer(
+                from,
+                non_existent_acc,
+                vec![create_coin(denom_basecoin, 100)?]
+            )
+            .is_ok());
+        assert_eq!(state.get_balances(from).unwrap()[denom_basecoin], 800);
+        assert_eq!(
+            state.get_balances(non_existent_acc).unwrap()[denom_basecoin],
+            100
+        );
+
+        // transfer from newly created account
+        let existent_acc = non_existent_acc;
+        assert!(state
+            .transfer(existent_acc, to, vec![create_coin(denom_basecoin, 100)?])
+            .is_ok());
+        assert_eq!(state.get_balances(existent_acc).unwrap()[denom_basecoin], 0);
+        assert_eq!(state.get_balances(to).unwrap()[denom_basecoin], 450);
+
+        // overflow
+        let pauper = "cosmos1wkvwnez6fkjn63xaz7nzpm4zxcd9cetqmyh2y8";
+        let ultra_high_net_worth_individual = "cosmos166vcha998g7tl8j8cq0kwa8rfvm68cqmj88cff";
+        assert_eq!(
+            state
+                .transfer(
+                    pauper,
+                    ultra_high_net_worth_individual,
+                    vec![create_coin(denom_basecoin, 1)?],
+                )
+                .err()
+                .unwrap()
+                .downcast::<StateError>()?,
+            StateError::DestFundOverflow
+        );
+
+        // multi-coin transfer
+        let denom_othercoin = "othercoin";
+        assert!(state
+            .transfer(
+                from,
+                to,
+                vec![
+                    create_coin(denom_basecoin, 100)?,
+                    create_coin(denom_othercoin, 100)?
+                ]
+            )
+            .is_ok());
+        assert_eq!(state.get_balances(from).unwrap()[denom_basecoin], 700);
+        assert_eq!(state.get_balances(to).unwrap()[denom_basecoin], 550);
+        assert_eq!(state.get_balances(from).unwrap()[denom_othercoin], 900);
+        assert_eq!(state.get_balances(to).unwrap()[denom_othercoin], 5100);
+
+        // multi-coin transfer with failure
+        assert_eq!(
+            state
+                .transfer(
+                    from,
+                    to,
+                    vec![
+                        create_coin(denom_basecoin, 10)?,
+                        create_coin("unknowncoin", 10)?
+                    ],
+                )
+                .err()
+                .unwrap()
+                .downcast::<StateError>()?,
+            StateError::InsufficientSourceFunds
+        );
+        assert_eq!(state.get_balances(from).unwrap()[denom_basecoin], 700);
+        assert_eq!(state.get_balances(to).unwrap()[denom_basecoin], 550);
+
+        Ok(())
+    }
+
+    // TODO(hu55a1n1): Add quickcheck test that checks equality of pre/post state after transfer
 }
