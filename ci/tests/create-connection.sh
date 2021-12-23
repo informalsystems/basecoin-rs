d8d8220ed33febcfde39df549d97d7ce8f66298d
Thane Thomson <connect@thanethomson.com>
2021-12-23 09:33:34 -0500

    Create bidirectional connection in integration test
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- ci/tests/create-connection.sh
+++ ci/tests/create-connection.sh
@@ -6,3 +6,6 @@ HERMES_BIN=${HERMES_BIN:-${HOME}/build/ibc-rs/release/hermes}
 echo "Creating connection between ibc-0 and basecoin-0..."
 "${HERMES_BIN}" create connection ibc-0 basecoin-0
 
+echo "Creating connection between basecoin-0 and ibc-0..."
+"${HERMES_BIN}" create connection basecoin-0 ibc-0
+
