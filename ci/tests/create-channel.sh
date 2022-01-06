c91a85e401f1cbb0f1047daa88a8d59bd4c038f8
Shoaib Ahmed <sufialhussaini@gmail.com>
2022-01-06 16:40:25 +0530

    Fix typo

--- ci/tests/create-channel.sh
+++ ci/tests/create-channel.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 HERMES_BIN=${HERMES_BIN:-${HOME}/build/ibc-rs/release/hermes}
 
-echo "Creating connection between ibc-0 and basecoin-0..."
+echo "Creating channel between ibc-0 and basecoin-0..."
 "${HERMES_BIN}" create channel ibc-0 basecoin-0 --port-a transfer --port-b transfer
 
-echo "Creating connection between basecoin-0 and ibc-0..."
+echo "Creating channel between basecoin-0 and ibc-0..."
 "${HERMES_BIN}" create channel basecoin-0 ibc-0 --port-a transfer --port-b transfer
 
