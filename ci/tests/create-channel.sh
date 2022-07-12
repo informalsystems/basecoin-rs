f6b7080c0457092de039eac3344d3a13c56f7d2c
hu55a1n1 <sufialhussaini@gmail.com>
2022-07-12 11:40:46 +0200

    Update CI test for recent hermes CLI changes

--- ci/tests/create-channel.sh
+++ ci/tests/create-channel.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 HERMES_BIN=${HERMES_BIN:-${HOME}/build/ibc-rs/release/hermes}
 
 echo "Creating channel between ibc-0 and basecoin-0..."
-"${HERMES_BIN}" create connection ibc-0 basecoin-0
-"${HERMES_BIN}" create channel ibc-0 --port-a transfer --port-b transfer connection-0
+"${HERMES_BIN}" create connection --a-chain ibc-0 --b-chain basecoin-0
+"${HERMES_BIN}" create channel --a-chain ibc-0 --a-port transfer --b-port transfer --a-connection connection-0
 
 echo "Creating channel between basecoin-0 and ibc-0..."
-"${HERMES_BIN}" create connection basecoin-0 ibc-0
-"${HERMES_BIN}" create channel basecoin-0 --port-a transfer --port-b transfer connection-1
+"${HERMES_BIN}" create connection --a-chain basecoin-0 --b-chain ibc-0
+"${HERMES_BIN}" create channel --a-chain basecoin-0 --a-port transfer --b-port transfer --a-connection connection-1
