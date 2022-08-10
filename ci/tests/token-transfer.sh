21c23d92e463281a7b18aad25feb59a102ec442d
hu55a1n1 <sufialhussaini@gmail.com>
2022-08-10 15:12:57 +0200

    Update token-transfer.sh for hermes CLI changes

--- ci/tests/token-transfer.sh
+++ ci/tests/token-transfer.sh
@@ -5,9 +5,9 @@ HERMES_BIN=${HERMES_BIN:-${HOME}/build/ibc-rs/release/hermes}
 
 echo "Testing token transfer..."
 
-"${HERMES_BIN}" tx raw ft-transfer --dst-chain ibc-0 --src-chain basecoin-0 --src-port transfer --src-channel channel-0 \
+"${HERMES_BIN}" tx ft-transfer --dst-chain ibc-0 --src-chain basecoin-0 --src-port transfer --src-channel channel-0 \
   --amount 9999 --timeout-height-offset 1000 --number-msgs 2
-"${HERMES_BIN}" tx raw ft-transfer --dst-chain basecoin-0 --src-chain ibc-0 --src-port transfer --src-channel channel-1 \
+"${HERMES_BIN}" tx ft-transfer --dst-chain basecoin-0 --src-chain ibc-0 --src-port transfer --src-channel channel-1 \
   --amount 9999 --timeout-height-offset 1000 --number-msgs 2
 
 timeout 10 "${HERMES_BIN}" start || [[ $? -eq 124 ]]
