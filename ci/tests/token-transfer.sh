d1a80e12002acd1dac5013f90a0c7c78dfec2d85
Shoaib Ahmed <sufialhussaini@gmail.com>
2022-08-11 15:51:29 +0200

    CI test for token transfer impl (#65)
    * Update ibc-rs deps to branch 'basecoin/phase6'
    
    * Impl Ics20 traits for IbcTransferModule
    
    * Implement module callbacks for Ibc
    
    * Replace BankKeeper::set_balances() with mint_coins() & impl send_coins
    
    * Impl mint_coins()
    
    * Impl burn_coins()
    
    * Add bank keeper to Ibc transfer module
    
    * Check for sufficient balance before burning tokens
    
    * Make Denom inner string public
    
    * Use U256 amounts
    
    * Update Cargo.lock
    
    * Remove assoc type `BankKeeper::Coins`
    
    * Require `BankKeeper::Error: Debug` & `BankKeeper::Address: FromStr`
    
    * Fix missing check for sufficient balance in `BankKeeper::send_coins()`
    
    * Impl ICS20's `BankKeeper` for `IbcTransferModule`
    
    * Update config staking denom
    
    * Use non-zero `historical_entries` to pass hermes's health check
    
    * Implement bank gRPC service
    
    * Implement IBC `ClientQuery::client_states`
    
    * Implement IBC `ConnectionQuery::client_connections`
    
    * Fix merge issues
    
    * Fix clippy errors
    
    * Return ChannelError::packet_receipt_not_found if receipt is not found
    
    * Update IBC patch deps to use the master branch
    
    * Implement transfer msg service
    
    * Handler deliverTx for MsgTransfer
    
    * Custom get_channel_escrow_address() impl
    
    * Fix send_coins when dest doesn't have specified denom
    
    * Implement BinCodec
    
    * Use BinStore for PacketCommitment & AcknowledgementCommitment
    
    * Don't delete PacketCommitment in delete_packet_commitment(), instead just set it to `vec![]`
    
    * Add missing check for matching portchan in `ChannelQuery::packet_commitments()`
    
    * Add missing check for empty commitments in `ChannelQuery::packet_commitments()`
    
    * Fix `ChannelQuery::packet_commitments()`
    
    * Update ibc-rs patch branch to `hu55a1n1/2386-fix-ics20-ds-serde`
    
    * Update CI ibc-rs patch branch to `hu55a1n1/2386-fix-ics20-ds-serde`
    
    * Update CI scripts for recent hermes CLI changes
    
    * Bump CI rust version
    
    * Update CI test for recent hermes CLI changes
    
    * Cleanup
    
    * Reformat
    
    * Fix comment typo
    
    * Fix delete_packet_acknowledgement() impl
    
    * Use `into_inner()` instead of `get_ref()` for tonic request conversions
    
    * Use `revision_number` '0' as default for consistency
    
    * Fix `packet_commitments` & `packet_acknowledgements` impls
    
    * Remove unused `TypedStore::delete()` method
    
    * Remove raw store fields
    
    * Remove unused transfer service
    
    * Fix missing `host_height` impl
    
    * Define const CHAIN_REVISION_NUMBER
    
    * Remove ConnectionEndWrapper
    
    * More cleanup
    
    * Update ibc deps
    
    * Use cosmos_adr028_escrow_address() from ibc crate
    
    * Use ibc-rs master in CI tests
    
    * Set up entrypoint.sh to run all tests in `TEST_DIR` by default
    
    * Fix create-channel.sh logs
    
    * Add token transfer test
    
    * Fix genesis.json to allocate samoleans to user account
    
    * Bump version
    
    * Fix CI failure due to timeout ret code
    
    * Update token-transfer.sh for hermes CLI changes

---
+++ ci/tests/token-transfer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+HERMES_BIN=${HERMES_BIN:-${HOME}/build/ibc-rs/release/hermes}
+
+echo "Testing token transfer..."
+
+"${HERMES_BIN}" tx ft-transfer --dst-chain ibc-0 --src-chain basecoin-0 --src-port transfer --src-channel channel-0 \
+  --amount 9999 --timeout-height-offset 1000 --number-msgs 2
+"${HERMES_BIN}" tx ft-transfer --dst-chain basecoin-0 --src-chain ibc-0 --src-port transfer --src-channel channel-1 \
+  --amount 9999 --timeout-height-offset 1000 --number-msgs 2
+
+timeout 10 "${HERMES_BIN}" start || [[ $? -eq 124 ]]
+
+"${HERMES_BIN}" query packet pending-sends --chain ibc-0 --port transfer --channel channel-0
+"${HERMES_BIN}" query packet pending-sends --chain basecoin-0 --port transfer --channel channel-1
+"${HERMES_BIN}" query packet pending-acks --chain basecoin-0 --port transfer --channel channel-0
+"${HERMES_BIN}" query packet pending-acks --chain ibc-0 --port transfer --channel channel-1
