d30cbab61034ca3af13247c5a477016418e75c09
Philippe Laferriere <plafer@protonmail.com>
2023-03-06 09:52:46 -0500

    use IbcModule as name

--- src/modules/ibc/router.rs
+++ src/modules/ibc/router.rs
@@ -1,23 +1,23 @@
 use std::{borrow::Borrow, collections::BTreeMap, sync::Arc};
 
-use ibc::core::ics26_routing::context::{Module, ModuleId};
+use ibc::core::ics26_routing::context::{Module as IbcModule, ModuleId};
 
 #[derive(Clone, Default, Debug)]
-pub struct IbcRouter(pub BTreeMap<ModuleId, Arc<dyn Module>>);
+pub struct IbcRouter(pub BTreeMap<ModuleId, Arc<dyn IbcModule>>);
 
 impl IbcRouter {
-    pub fn get_route(&self, module_id: &impl Borrow<ModuleId>) -> Option<&dyn Module> {
+    pub fn get_route(&self, module_id: &impl Borrow<ModuleId>) -> Option<&dyn IbcModule> {
         self.0.get(module_id.borrow()).map(Arc::as_ref)
     }
 
-    pub fn get_route_mut(&mut self, module_id: &impl Borrow<ModuleId>) -> Option<&mut dyn Module> {
+    pub fn get_route_mut(&mut self, module_id: &impl Borrow<ModuleId>) -> Option<&mut dyn IbcModule> {
         self.0.get_mut(module_id.borrow()).and_then(Arc::get_mut)
     }
 
     pub fn add_route(
         &mut self,
         module_id: ModuleId,
-        module: impl Module,
+        module: impl IbcModule,
     ) -> Result<(), String> {
         match self.0.insert(module_id, Arc::new(module)) {
             None => Ok(()),
