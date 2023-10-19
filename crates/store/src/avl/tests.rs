2f44abeeef08af32b6dfbcabe83dccf5be9c8859
Ranadeep Biswas <mail@rnbguy.at>
2023-10-19 11:10:35 +0200

    mv test to tests file

--- crates/store/src/avl/tests.rs
+++ crates/store/src/avl/tests.rs
@@ -9,6 +9,9 @@ use crate::avl::{
     *,
 };
 
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
 #[test]
 fn insert() {
     let data = [42];
@@ -35,6 +38,25 @@ fn get() {
     assert_eq!(tree.get(&[2]), Some(&[2]));
     assert_eq!(tree.get(&[5]), Some(&[5]));
     assert_eq!(tree.get(&[4]), None);
+}
+
+#[test]
+fn shuffle_remove() {
+    let mut tree = AvlTree::new();
+
+    let mut keys: Vec<u8> = (0..100).collect();
+
+    keys.shuffle(&mut thread_rng());
+    for &i in keys.iter() {
+        tree.insert([i], vec![i]);
+    }
+
+    keys.shuffle(&mut thread_rng());
+    for &i in keys.iter() {
+        assert_eq!(tree.remove([i]), Some(vec![i]));
+    }
+
+    assert_eq!(tree.root, None);
 }
 
 #[test]
