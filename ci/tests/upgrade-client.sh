454fe8f1ef1ad9ec4606e9313b7a88bf34d06f34
Farhad Shabani <farhad.shabani@gmail.com>
2024-02-20 14:32:48 -0800

    fix: keep upgrade-client script as is

--- ci/tests/upgrade-client.sh
+++ ci/tests/upgrade-client.sh
@@ -17,6 +17,6 @@ hermes tx upgrade-chain --reference-chain ibc-0 --host-chain basecoin-0 --host-c
 sleep 3s
 gaiad --node tcp://localhost:26657 tx gov vote 1 yes --home $HOME/data/ibc-0/data --keyring-backend test --keyring-dir $HOME/data/ibc-0 --chain-id ibc-0 --from validator --yes
 sleep 3s
-plan_height=$(gaiad --node tcp://localhost:26657 query gov proposal 1 --home $HOME/data/ibc-0/data | grep -o '"height":"[0-9]*"' | cut -d':' -f2 | tr -d '"')
+plan_height=$(gaiad --node tcp://localhost:26657 query gov proposal 1 --home $HOME/data/ibc-0/data | grep ' height:' | awk '{print $2}' | tr -d '"')
 echo "Waiting for upgrade plan to execute at height $plan_height..."
 hermes upgrade client --host-chain basecoin-0 --client 07-tendermint-0 --upgrade-height $plan_height
