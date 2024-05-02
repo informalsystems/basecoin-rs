c4137168bfe80a2ac10ab205bbc98f48ce1f3342
Ranadeep Biswas <mail@rnbguy.at>
2024-05-02 17:34:06 +0200

    make features additive

--- basecoin/app/src/abci/mod.rs
+++ basecoin/app/src/abci/mod.rs
@@ -1,8 +1,8 @@
 //! Contains various ABCI implementations to interact with different version of
 //! underlying consensus engine (CometBFT)
 
-#[cfg(all(feature = "v0_37", not(feature = "v0_38")))]
+#[cfg(feature = "v0_37")]
 pub mod v0_37;
 
-#[cfg(any(feature = "v0_38", not(feature = "v0_37")))]
+#[cfg(feature = "v0_38")]
 pub mod v0_38;
