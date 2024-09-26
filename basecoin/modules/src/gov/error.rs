80286df795159d7ccca669588398dea5870d7be1
Ranadeep Biswas <mail@rnbguy.at>
2024-09-26 11:07:50 +0200

    new error variant for Gov

--- basecoin/modules/src/gov/error.rs
+++ basecoin/modules/src/gov/error.rs
@@ -6,6 +6,10 @@ pub use crate::error::Error as AppError;
 pub enum Error {
     /// invalid proposal: `{reason}`
     InvalidProposal { reason: String },
+    /// failed to validate: `{reason}`
+    ValidationFailure { reason: String },
+    /// failed to execute: `{reason}`
+    ExecutionFailure { reason: String },
 }
 
 impl From<Error> for AppError {
